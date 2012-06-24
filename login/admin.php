a24c18693f9f59e95784e35e3d4547d0ea8902d3
mylabsql <mylabsql@gmail.com>
2012-06-24 07:43:42 +0700

    Revert "readme"
    This reverts commit 0e5f4f7cae4b3588d36ffa93c149d1ff764da39b.

--- login/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-?><div align="center" style="color:green;">
-<?php
-echo "Welcome " .$_SESSION['name'];
-?>
-<br /><a href="logout.php">Logout</a>
-</div>
--- login/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nameSer = "localhost";
-$username ="root";
-$pass = "";
-$dbname = "tehnik";
-$koneksi = mysql_connect($names,$username,$pass)or die('error'.mysql_error());
-$selecdb = mysql_select_db($dbname);
-?>
